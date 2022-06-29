e26766d7a318fe594d242d07094d0f72d4ebeca7
Kseniia <ksenjadzjubina@gmail.com>
2022-06-30 01:33:28 +0300

    Added 3rd task

---
+++ Homework003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число  ");
+int numberA;
+numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else if (numberA %2 == 1)
+{
+    Console.WriteLine("Число нечётное");
+}
