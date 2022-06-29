516220152856b5c62c5359b0caa9820a491daca7
Kseniia <ksenjadzjubina@gmail.com>
2022-06-30 00:55:56 +0300

    Added first task

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите два числа для сравнения");
+int numberA, numberB;
+numberA = Convert.ToInt32(Console.ReadLine());
+numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("Максимальное число {0}", numberA);
+    Console.WriteLine("Минимальное число {0}", numberB);
+}
+else
+{
+    Console.WriteLine("Максимальное число {0}", numberB);
+    Console.WriteLine("Минимальное число {0}", numberA);
+
+};
+Console.ReadLine();
