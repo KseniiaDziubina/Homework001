5da7642d4311a391b6fb58c662fc5f06b492bc1d
Kseniia <ksenjadzjubina@gmail.com>
2022-06-30 01:16:06 +0300

    Added second task

---
+++ Homework002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите три числа для сравнения");
+int numberA, numberB, numberC;
+numberA = Convert.ToInt32(Console.ReadLine());
+numberB = Convert.ToInt32(Console.ReadLine());
+numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB && numberA > numberC)
+{
+    Console.WriteLine("Максимальное число {0}", numberA);
+}
+else if (numberB > numberC)
+{
+        Console.WriteLine("Максимальное число {0}", numberB);
+}
+else
+{
+        Console.WriteLine("Максимальное число {0}", numberC);
+};
+Console.ReadLine();
